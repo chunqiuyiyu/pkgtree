d2c90123de8f12cbce495c972f41821bf30b0154
chunqiuyiyu <chunqiuyiyu@outlook.com>
2018-09-20 08:59:36 +0800

    Add main logic

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# pkgtree
+# Pkgtree
+
+>Show dependency tree of node project.
+
+![preview](./preview.gif)
+
+## Installation
+```js
+npm install pkgtree -g
+```
+
+## Usage
+```shell
+pkgtree [input]
+
+Options:
+  
+  -l  show the tree with package version
+  -v  output the version number
+  -h  output usage information
+```
+
+## License
+MIT
--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,8 @@
 import test from 'ava'
-import fs from 'fs'
 import utils from '../lib/utils'
+// eslint-disable-next-line
+import child_process from 'child_process'
+import packageJson from '../package.json'
 
 test('Parse CLI arguments correctly.', t => {
   const parse = utils.parse
@@ -20,5 +22,15 @@ test('Check if file exists', t => {
 })
 
 test('Read file content', t => {
+  const readPackage = utils.readPackage
+  t.is(readPackage('test/test.md'), 'This is a test file.')
+})
 
+test('Output correct version', t => {
+  // eslint-disable-next-line
+  const { execSync } = child_process
+  t.is(
+    execSync('node lib/index.js -v').toString().replace('\n', ''),
+    packageJson.version
+  )
 })
